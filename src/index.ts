21afc1bd91160233bd415a9cab1aa9a1f5c19307
mashakrot <makrosensei@gmail.com>
2024-12-09 23:55:28 +0200

    agaaaain

--- src/index.ts
+++ src/index.ts
@@ -51,35 +51,130 @@ router.post('/upload', upload.single('image'), async (req: Request, res: Respons
   }
 
   try {
-    const imagePath = req.file?.path || '';
+    const imagePath = req.file?.path || ''; // Path to the uploaded image file
+
+    // Create and save a new offer
     const newOffer: IOffer = new Offer({ title, description, price, image: imagePath });
     await newOffer.save();
 
-    res.json({ message: 'Offer saved successfully!' });
+    res.status(201).json({ message: 'Offer saved successfully!' });
   } catch (error) {
     console.error('Error saving offer:', error);
-    res.json({ message: 'Internal server error. Failed to save offer.' });
+    res.status(500).json({ message: 'Internal server error. Failed to save offer.' });
   }
 });
 
-router.get("/offers", async (req: Request, res: Response) => {
-  try {
-    const offers = await Offer.find().populate("imageId").exec();
 
-    const responseOffers = offers.map((offer) => ({
-      title: offer.title,
-      description: offer.description,
-      price: offer.price,
-      imagePath: offer.imageId && "path" in offer.imageId ? offer.imageId.path : null,
-    }));
 
-    res.json(responseOffers);
-  } catch (error) {
-    console.error("Error fetching offers:", error);
-    res.json({ message: "Failed to fetch offers." });
-  }
-});
+// router.get('/todos/:id', async (req: Request, res: Response) => {
+//     const { name, id } = req.params;
+  
+//     // let user = await User.findOne({ name });
+//     const user = await User.findOne({ name: id });
 
+
+//     console.log(user)
+
+//     if (user) {        
+//         res.json({ todos: user.todos });
+//         console.log(user.todos);
+        
+//     } else {
+//         res.json({ message: 'User not found' });
+//     }
+//   });
+
+
+//   router.delete('/delete', async (req: Request, res: Response) => {
+//     const { name } = req.body;
+  
+//     if (!name) {
+//       res.json({ message: "Name is required." });
+//     }
+  
+//     try {
+//       if (name) {
+//         const result = await User.findOneAndDelete({ name });
+        
+//         if (!result) {
+//           res.json({ message: "User not found." });
+//         }
+        
+//         res.json({ message: 'User deleted successfully.' });
+//       }
+//     } catch (error) {
+//       res.json({ message: 'Error deleting user.', error });
+//     }
+//   });
+  
+
+// router.put('/update', async (req: Request, res: Response) => {
+//   const { name, todo }: { name: string; todo: string } = req.body;
+  
+//     if (!name || !todo) {
+//       res.json({ message: 'Name and todo are required' });
+//     }
+  
+//     try {
+//       const user = await User.findOne({ name });
+  
+//       if (!user) {
+//         res.json({ message: "User not found" });
+//       }
+  
+//       if (user) {
+
+//         const originalTodosLength = user.todos.length;
+//         user.todos = user.todos.filter((t) => t.todo !== todo);
+        
+//         if (user.todos.length === originalTodosLength) {
+//           res.json({ message: "Todo not found" });
+//         }
+        
+//         await user.save();
+        
+//         res.json({ message: `Todo deleted successfully for user ${name}.` });
+//       }
+//     } catch (error) {
+//       res.json({ message: "Error deleting todo.", error });
+//     }
+//   });
+
+//   router.put("/updateTodo", async (req: Request, res: Response) => {
+//     const { name, todo, checked }: { name: string; todo: string; checked: boolean } = req.body;
+  
+//     if (!name || !todo || typeof checked !== "boolean") {
+//       res.json({ message: "Name, todo, and checked are required." });
+//     }
+  
+//     try {
+//       const user = await User.findOne({ name });
+  
+//       if (!user) {
+//         res.json({ message: "User not found." });
+//       }
+  
+//       if (user) {
+        
+//         const targetTodo = user.todos.find((t) => t.todo === todo);
+        
+//         if (!targetTodo) {
+//           res.json({ message: "Todo not found." });
+//         }
+        
+//         if (targetTodo) {
+//           targetTodo.checked = checked;
+//         }
+        
+//         await user.save();
+        
+//         res.json({ message: "Todo updated successfully." });
+//       }
+//     } catch (error) {
+//       res.json({ message: "Error updating todo.", error });
+//     }
+//   });
+    
 
 
 export default router
